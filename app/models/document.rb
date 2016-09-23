d694a3110a57100a74586e7758dd1c4e7280af2f
Drew Cimino <drewcimino@gmail.com>
2016-09-23 15:41:28 +0300

    #tag_matches to produce sets of Tag label/context data from raw document text

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,15 @@
 class Document < ActiveRecord::Base
   validates :provided_tags, :content, presence: true
+
+  def tag_matches(tag_phrase)
+    scan_text    = content.dup
+    matched_tags = []
+
+    while match_data = scan_text.match(/([\.\?\!])?[^\.\?\!]*#{tag_phrase}[^\.\?\!]*[\.\?\!]*[^\.\?\!]*[\.\?\!]*/i)
+      matched_tags << { label: tag_phrase, context: match_data[0].sub(/^[\.\?\!]/,'').strip }
+      scan_text.sub! /#{tag_phrase}/, ''
+    end
+
+    matched_tags
+  end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -23,4 +23,32 @@ describe Document do
       it { is_expected.to be_invalid }
     end
   end
+
+  describe '#tag_matches' do
+    let(:tag_phrase) { 'README' }
+    subject(:matches) { document.tag_matches(tag_phrase) }
+
+    context 'when the provided tag is not present' do
+      let(:tag_phrase) { 'potato' }
+      it { is_expected.to eq [] }
+    end
+
+    context 'when the provided tag is present' do
+      it { is_expected.to eq [{ label: tag_phrase, context: 'Sample README Content? Active Record has more than one layer.' }] }
+    end
+
+    context 'when the provided tag occurs multiple times' do
+      let(:params) do
+        {
+          provided_tags: 'layer, Active Record, README',
+          content: 'Sample README Content? The Active Record README has more than one layer. Some other words about README.'
+        }
+      end
+
+      describe '#count' do
+        subject(:match_count) { matches.count }
+        it { is_expected.to eq params[:content].scan(tag_phrase).count }
+      end
+    end
+  end
 end
